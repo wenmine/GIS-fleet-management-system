e906295312cbf23385fa493c05ac0f676fe221b7
wenmine <wangwenmine@gmail.com>
2016-03-19 22:03:23 +0800

    添加船队form表单验证完成

--- web/js/fleet.js
+++ web/js/fleet.js
@@ -2,32 +2,33 @@
  * Created by Administrator on 2016/3/14.
  */
 var getCheckObject = function () {
-    var tipNum = tip = document.createElement("p");
-    tip.appendChild(document.createTextNode("必须为9位数字组成"));
-    var tipCall = tip = document.createElement("p");
-    tip.appendChild(document.createTextNode("必须为4位数字组成"));
-    var tipOther = tip = document.createElement("p");
-    tip.appendChild(document.createTextNode("输入框不能为空"));
+    var tipNum = document.createElement("span");
+    tipNum.className = "error";
+    tipNum.appendChild(document.createTextNode("必须为9位数字组成"));
+    var tipCall = document.createElement("span");
+    tipCall.className = "error";
+    tipCall.appendChild(document.createTextNode("必须为4位数字组成"));
 
     function addErrorTip(node, name) {
-        node.className += ' ' + 'error' + ' ';
+
         if (name === "number") {
             node.parentNode.appendChild(tipNum);
         } else if (name === "call_sign") {
             node.parentNode.appendChild(tipCall);
         } else {
+            if (node.nextElementSibling !== null) {
+                return;
+            }
+            var tipOther = document.createElement("span");
+            tipOther.className = "error";
+            tipOther.appendChild(document.createTextNode("输入框不能为空"));
             node.parentNode.appendChild(tipOther);
         }
     }
 
-    function removeErrorTip(node, name) {
-        node.className = "";
-        if (name === "number") {
-            node.parentNode.removeChild(tipNum);
-        } else if (name === "call_sign") {
-            node.parentNode.removeChild(tipCall);
-        } else {
-            node.parentNode.removeChild(tipOther);
+    function removeErrorTip(node) {
+        if (node.nextElementSibling !== null) {
+            node.parentNode.removeChild(node.nextElementSibling);
         }
     }
 
@@ -39,7 +40,7 @@ var getCheckObject = function () {
 
     function isValidCall(callvalue) {
         var reg = new RegExp("^[0-9]{4}$");
-        var hasEnouthDigital = reg.test(value);
+        var hasEnouthDigital = reg.test(callvalue);
         return hasEnouthDigital;
     }
 
@@ -53,38 +54,87 @@ var getCheckObject = function () {
 var checkObj = getCheckObject();
 
 //每次打开对话框时将里面的原先的值清空
-function doReset() {
-    for (i = 0; i < document.all.tags("input").length; i++) {
-        if (document.all.tags("input")[i].type == "text") {
-            document.all.tags("input")[i].value = "";
+function clearForm() {
+    var objId = document.getElementById("add-ship-form");
+    if (objId == undefined) {
+        return;
+    }
+    for (var i = 0; i < objId.elements.length; i++) {
+        if (objId.elements[i].type == "text") {
+            objId.elements[i].value = "";
+        }
+        else if (objId.elements[i].type == "password") {
+            objId.elements[i].value = "";
+        }
+        else if (objId.elements[i].type == "radio") {
+            objId.elements[i].checked = false;
+        }
+        else if (objId.elements[i].type == "checkbox") {
+            objId.elements[i].checked = false;
+        }
+        else if (objId.elements[i].type == "select-one") {
+            objId.elements[i].options[0].selected = true;
+        }
+        else if (objId.elements[i].type == "select-multiple") {
+            for (var j = 0; j < objId.elements[i].options.length; j++) {
+                objId.elements[i].options[j].selected = false;
+            }
+        }
+        else if (objId.elements[i].type == "textarea") {
+            objId.elements[i].value = "";
+        }
+    }
+}
+
+function iterateInputAddFocusListener() {
+    var addShip = document.getElementById("add-ship");
+    var inputList = addShip.getElementsByTagName('input');
+    var i = 0;
+    var shipNum = document.getElementById("number");
+    var shipCall = document.getElementById("call-sign");
+    shipNum.onblur = function () {
+        if (!checkObj.isValidNum(shipNum.value)) {
+            checkObj.addErrorTip(shipNum, 'number');
+        }
+    };
+    shipNum.onfocus = function () {
+        checkObj.removeErrorTip(shipNum);
+    };
+    shipCall.onblur = function () {
+        if (!checkObj.isValidCall(shipCall.value)) {
+            checkObj.addErrorTip(shipCall, 'call_sign');
+        }
+    };
+    shipCall.onfocus = function () {
+        checkObj.removeErrorTip(shipCall);
+    };
+    for (i = 0; i < inputList.length; i++) {
+        checkObj.removeErrorTip(inputList[i]);
+        if (inputList[i].name === "number" ||
+            inputList[i].name === "call_sign") {
+            continue;
+        } else {
+            inputList[i].onfocus = function () {
+                checkObj.removeErrorTip(this);
+            };
+            inputList[i].onblur = function() {
+                if (this.value == "") {
+                    nullFlag = false;
+                    checkObj.addErrorTip(this);
+                }
+            }
         }
     }
 }
 
 function wOpen() {
     document.getElementById("add-ship").style.display = "block";
-    var shipNum = document.getElementById("number");
-    var shipCall = document.getElementById("call-sign");
-    shipNum.addEventListener('blur', function (event) {
-        if (!checkObj.isValidNum(shipNum.value)) {
-            checkObj.addErrorTip(shipNum, 'number');
-        }
-    }, false);
-    shipNum.addEventListener('focus', function (event) {
-        checkObj.removeErrorTip(shipNum, 'number');
-    }, false);
-    shipCall.addEventListener('blur', function (event) {
-        if (!checkObj.isValidCall(shipCall.value)) {
-            checkObj.addErrorTip(shipCall, 'call_sign');
-        }
-    }, false);
-    shipCall.addEventListener('focus', function (event) {
-        checkObj.removeErrorTip(shipCall, 'call_sign');
-    }, false);
+    $("#add-ship").css("display", "block");
+    iterateInputAddFocusListener();
 }
 function wClose(that) {
     that.parentNode.parentNode.style.display = "none";
-    doReset();
+    clearForm();
 }
 function editShipHandler(num, event) {
     document.getElementById("add-ship").style.display = "block";
@@ -97,6 +147,7 @@ function delShipHandler(shipElement, event) {
     shipElement = null;
     stopEvent(event);
 }
+
 function addShipSubmitCheck() {
     var addShip = document.getElementById("add-ship");
     var inputList = addShip.getElementsByTagName('input');
@@ -104,12 +155,8 @@ function addShipSubmitCheck() {
     var nullFlag = true;
     var numFlag = true;
     var callFlag = true;
-
     for (i = 0; i < inputList.length; i++) {
-        if (inputList[i].value == "") {
-            nullFlag = false;
-            checkObj.addErrorTip(inputList[i]);
-        } else if (inputList[i].name === "number") {
+        if (inputList[i].name === "number") {
             numFlag = checkObj.isValidNum(inputList[i].value);
             if (!numFlag) {
                 checkObj.addErrorTip(inputList[i], 'number');
@@ -121,12 +168,15 @@ function addShipSubmitCheck() {
                 checkObj.addErrorTip(inputList[i], 'call_sign');
                 event.preventDefault();
             }
+        } else if (inputList[i].value == "") {
+            nullFlag = false;
+            checkObj.addErrorTip(inputList[i]);
         }
     }
     return (nullFlag && numFlag && callFlag);
 }
 function addShipSubmit() {
-    if(!addShipSubmitCheck()){
+    if (!addShipSubmitCheck()) {
         return;
     }
     document.getElementById("add-ship").style.display = "none";
@@ -140,7 +190,7 @@ function addShipSubmit() {
     console.log(shipElement);
     if (shipList.childNodes) {
         if (shipList.childNodes[0])
-            shipList.insertBefore(shipElement, shipList.lastChild.nextSibling);
+            shipList.insertBefore(shipElement, shipList.lastChild.nextElementSibling);
     }
     else {
         shipList.appendChild(shipElement);
@@ -149,12 +199,12 @@ function addShipSubmit() {
     shipSpan1.className = "edit";
     shipSpan2.className = "del";
     shipElement.appendChild(shipA);
-    shipElement.insertBefore(shipSpan1, shipElement.lastChild.nextSibling);
-    shipElement.insertBefore(shipSpan2, shipElement.lastChild.nextSibling);
+    shipElement.insertBefore(shipSpan1, shipElement.lastChild.nextElementSibling);
+    shipElement.insertBefore(shipSpan2, shipElement.lastChild.nextElementSibling);
     num = shipNum.value;
     shipA.innerHTML = shipNum.value;
     if (shipElement) {
-        shipElement.onclick = function(){
+        shipElement.onclick = function () {
             stopEvent(event);
         };
     }
@@ -168,10 +218,5 @@ function addShipSubmit() {
             delShipHandler(shipElement, event);
         };
     }
-    doReset();
+    clearForm();
 }
-
-//function cancelShipSubmit(){
-//    document.getElementById("add-ship").style.display = "none";
-//    doReset();
-//}

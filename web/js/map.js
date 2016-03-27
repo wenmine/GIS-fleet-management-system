983eb388d686512166375d9511904e636938c43e
wenmine <wangwenmine@gmail.com>
2016-03-27 20:59:27 +0800

    加了添加标注文字信息的

--- web/js/map.js
+++ web/js/map.js
@@ -1,11 +1,11 @@
 var projection = ol.proj.get("EPSG:3857");//指定了地图切片使用的坐标系
 var resolutions = [];
-var draw1=null;//定义全局变量，当绘图方式改变时删除当前的绘制工具 
-var draw2=null;
+var draw1 = null;//定义全局变量，当绘图方式改变时删除当前的绘制工具
+var draw2 = null;
 var map = null;
 var source = null;
 var fleet_source = null;
-var  range_source = null;
+var range_source = null;
 window.onload = function () {
     var format = 'image/png';
     var bounds = [73.4510046356223, 18.1632471876417,
@@ -22,33 +22,33 @@ window.onload = function () {
     //缩放控件
     var zoomSliderControl = new ol.control.ZoomSlider({});
     controls.push(zoomSliderControl);
-    for(var i=0; i<19; i++){
-        resolutions[i] = Math.pow(2, 18-i);
+    for (var i = 0; i < 19; i++) {
+        resolutions[i] = Math.pow(2, 18 - i);
     }
-    var tilegrid  = new ol.tilegrid.TileGrid({
-        origin: [0,0],
+    var tilegrid = new ol.tilegrid.TileGrid({
+        origin: [0, 0],
         resolutions: resolutions
     });//指定了切片使用的网络的模式
 
     var baidu_source = new ol.source.TileImage({
         projection: projection,
         tileGrid: tilegrid,
-        tileUrlFunction: function(tileCoord, pixelRatio, proj){//每次用户与地图交互，比如 缩放、平移等，就会触发会触发回调函数，此函数根据传入的变量参数构造图片切片的 URL 地址。
-            if(!tileCoord){
+        tileUrlFunction: function (tileCoord, pixelRatio, proj) {//每次用户与地图交互，比如 缩放、平移等，就会触发会触发回调函数，此函数根据传入的变量参数构造图片切片的 URL 地址。
+            if (!tileCoord) {
                 return "";
             }
             var z = tileCoord[0];//当前缩放级别z
             var x = tileCoord[1];//切片的x索引
             var y = tileCoord[2];//切片的y索引
 
-            if(x<0){
-                x = "M"+(-x);
+            if (x < 0) {
+                x = "M" + (-x);
             }
-            if(y<0){
-                y = "M"+(-y);
+            if (y < 0) {
+                y = "M" + (-y);
             }
 
-            return "http://online3.map.bdimg.com/onlinelabel/?qt=tile&x="+x+"&y="+y+"&z="+z+"&styles=pl&udt=20160222&scaler=1&p=1";
+            return "http://online3.map.bdimg.com/onlinelabel/?qt=tile&x=" + x + "&y=" + y + "&z=" + z + "&styles=pl&udt=20160222&scaler=1&p=1";
         }
     });
 
@@ -57,40 +57,52 @@ window.onload = function () {
     });
     //创建标注图层的样式
     var iconStyle = new ol.style.Style({
-        fill:new ol.style.Fill({
-            color:'rgba(255,255,255,0.2)'
+        fill: new ol.style.Fill({
+            color: 'rgba(255,255,255,0.2)'
         }),
-        stroke:new ol.style.Stroke({
-            color:'#0c95f8',
-            width:2
+        stroke: new ol.style.Stroke({
+            color: '#0c95f8',
+            width: 2
         }),
-        image: new ol.style.Icon( ({
+        image: new ol.style.Icon(({
             anchor: [0.5, 46],
             anchorXUnits: 'fraction',
             anchorYUnits: 'pixels',
             opacity: 0.75,
             src: 'http://openlayers.org/en/v3.9.0/examples/data/icon.png'
-        }))
+        })),
+        text: new ol.style.Text({
+            text: "Wow such label",
+            offsetY: 0,
+            textAlign: 'left',
+            fill: new ol.style.Fill({
+                color: 'rgba(255,255,255,0.2)'
+            }),
+            stroke: new ol.style.Stroke({
+                color: '#ff0000',
+                width: 1
+            })
+        })
     });
 
     //临时标注图层的数据源
-    source=new ol.source.Vector();
+    source = new ol.source.Vector();
     //新建临时标注图层，并设置临时图层渲染各种要素的样式
-    var vector=new ol.layer.Vector({
-        source:source,
+    var vector = new ol.layer.Vector({
+        source: source,
         style: iconStyle
     });
 
     //创建船队图层的样式
     var fleetStyle = new ol.style.Style({
-        fill:new ol.style.Fill({
-            color:'rgba(255,255,255,0.2)'
+        fill: new ol.style.Fill({
+            color: 'rgba(255,255,255,0.2)'
         }),
-        stroke:new ol.style.Stroke({
-            color:'#0c95f8',
-            width:2
+        stroke: new ol.style.Stroke({
+            color: '#0c95f8',
+            width: 2
         }),
-        image: new ol.style.Icon( ({
+        image: new ol.style.Icon(({
             anchor: [0.5, 46],
             anchorXUnits: 'fraction',
             anchorYUnits: 'pixels',
@@ -100,30 +112,30 @@ window.onload = function () {
     });
 
     //临时标注图层的数据源
-    fleet_source=new ol.source.Vector();
+    fleet_source = new ol.source.Vector();
     //新建临时标注图层，并设置临时图层渲染各种要素的样式
-    var fleet_vector=new ol.layer.Vector({
-        source:fleet_source,
+    var fleet_vector = new ol.layer.Vector({
+        source: fleet_source,
         style: fleetStyle
     });
     //临时测距图层的数据源
     range_source = new ol.source.Vector();
     //新建临时测距图层，并设置临时图层渲染各种要素的样式
     var range_vector = new ol.layer.Vector({
-        source:range_source,
-        style:new ol.style.Style()
+        source: range_source,
+        style: new ol.style.Style()
     });
     map = new ol.Map({
         controls: ol.control.defaults({
-                    attribution: false
-                }).extend(controls),
-                interactions: ol.interaction.defaults().extend([
-                    new ol.interaction.DragRotateAndZoom()
-                ]),
+            attribution: false
+        }).extend(controls),
+        interactions: ol.interaction.defaults().extend([
+            new ol.interaction.DragRotateAndZoom()
+        ]),
         target: 'map',
-        layers: [baidu_layer,vector,range_vector,fleet_vector],
+        layers: [baidu_layer, vector, range_vector, fleet_vector],
         view: new ol.View({
-            center:  [12959773,4853101],//设置地图中心
+            center: [12959773, 4853101],//设置地图中心
             zoom: 5 //初始的缩放等级
         })
     });
--- web/js/markmap.js
+++ web/js/markmap.js
@@ -8,36 +8,36 @@ var sketch;
  */
 var sketchElement;
 
-function setMarkUnFlod(ulname,name,count){
-    var links =  document.getElementById(ulname).getElementsByTagName('li');
-    var measurePopup =  document.getElementById("measure-popup");
+function setMarkUnFlod(ulname, name, count) {
+    var links = document.getElementById(ulname).getElementsByTagName('li');
+    var measurePopup = document.getElementById("measure-popup");
     measurePopup.style.display = "block";
-    for (var i=0;i<links.length;i++){
-        var menu =  document.getElementById("submenu-"+name+(i+1));
-        if(count == (i+1)&&menu){
-            if (  menu.style.display == "block"){
+    for (var i = 0; i < links.length; i++) {
+        var menu = document.getElementById("submenu-" + name + (i + 1));
+        if (count == (i + 1) && menu) {
+            if (menu.style.display == "block") {
                 menu.style.display = "none";
-                menu.parentNode.className ="list-title-up";
-                if(count == 3){
+                menu.parentNode.className = "list-title-up";
+                if (count == 3) {
                     measurePopup.style.display = "none";
-                    if(sketchElement){
-                        sketchElement.innerHTML ="";
+                    if (sketchElement) {
+                        sketchElement.innerHTML = "";
                     }
                 }
                 check();
-            }else if(menu.style.display == "none" || menu.style.display == ""){
+            } else if (menu.style.display == "none" || menu.style.display == "") {
                 menu.style.display = "block";
-                menu.parentNode.className ="list-title-down";
-                if(count == 3){
+                menu.parentNode.className = "list-title-down";
+                if (count == 3) {
                     measurePopup.style.display = "block";
-                    if(sketchElement){
-                        sketchElement.innerHTML ="";
+                    if (sketchElement) {
+                        sketchElement.innerHTML = "";
                     }
                 }
             }
             map.removeInteraction(draw1);
             map.removeInteraction(draw2);
-        }else if(menu){
+        } else if (menu) {
             menu.style.display = "none";
             measurePopup.style.display = "none";
 
@@ -45,60 +45,71 @@ function setMarkUnFlod(ulname,name,count){
     }
 }
 /*实现标注与测距功能*/
-function init(ulname,name,count){
+function init(ulname, name, count) {
     /*标注开始*/
-    var typeSelected=document.getElementById('drawtype');
+    var typeSelected = document.getElementById('drawtype');
     //初始化绘图工具
-    function addInteraction(){
-        var value=typeSelected.value;
-        if(value!=='None'){
-            draw1=new ol.interaction.Draw({
-                source:source,//设置要素源，绘制结束后将绘制的要素添加到临时图层
-                type:(value)//绘制的类型
+    function addInteraction() {
+        var value = typeSelected.value;
+        if (value !== 'None') {
+            draw1 = new ol.interaction.Draw({
+                source: source,//设置要素源，绘制结束后将绘制的要素添加到临时图层
+                type: (value)//绘制的类型
             });
             map.addInteraction(draw1);
+            draw1.on('drawend', function (evt) {
+                //console.log(evt.feature);
+                //var format = new ol.format.GeoJSON();
+                //format.writeFeature(evt.feature);
+                //var coord = evt.feature.getGeometry().getCoordinates();
+                //console.log(coord);
+                //var feature = new ol.Feature(new ol.geom.Point([12053737.17543087, 4402772.829226152]));
+                //source.addFeature(feature);
+                window.temp = evt.feature;
+            });
         }
     }
+
     //绘制方式改变后重新初始化绘图工具
-    typeSelected.onchange= function(e){
+    typeSelected.onchange = function (e) {
         map.removeInteraction(draw1);
         addInteraction();
     };
     /*测距开始*/
-    var viewport=map.getViewport();
-    var testType=document.getElementById('testtype');
-    var mouseMoveHandler=function(evt){
-        if(sketch){
+    var viewport = map.getViewport();
+    var testType = document.getElementById('testtype');
+    var mouseMoveHandler = function (evt) {
+        if (sketch) {
             var output;
-            var geom=(sketch.getGeometry());
-            if(geom instanceof ol.geom.Polygon){
-                output=formateArea(geom);
+            var geom = (sketch.getGeometry());
+            if (geom instanceof ol.geom.Polygon) {
+                output = formateArea(geom);
             }
-            else if(geom instanceof  ol.geom.LineString){
-                output=formateLength(geom);
+            else if (geom instanceof ol.geom.LineString) {
+                output = formateLength(geom);
             }
-            sketchElement.innerHTML=output;
+            sketchElement.innerHTML = output;
 
         }
     };
-    viewport.addEventListener('mousemove',mouseMoveHandler);
+    viewport.addEventListener('mousemove', mouseMoveHandler);
 
-    function addInteraction2(map,typeSelect,source){
-        var type=typeSelect.value=='area'?'Polygon':'LineString';
-        if(typeSelect.value!=='None'){
-            draw2=new ol.interaction.Draw({
-                source:range_source,
-                type:(type)
+    function addInteraction2(map, typeSelect, source) {
+        var type = typeSelect.value == 'area' ? 'Polygon' : 'LineString';
+        if (typeSelect.value !== 'None') {
+            draw2 = new ol.interaction.Draw({
+                source: range_source,
+                type: (type)
 
             });
             map.addInteraction(draw2);
         }
-        draw2.on('drawstart',function(evt){
+        draw2.on('drawstart', function (evt) {
 
-            sketch=evt.feature;
-            sketchElement=document.getElementById('measureOutput');
+            sketch = evt.feature;
+            sketchElement = document.getElementById('measureOutput');
         });
-        draw2.on('drawend',function(evt){
+        draw2.on('drawend', function (evt) {
             //console.log(evt.feature);
             var format = new ol.format.GeoJSON();
             format.writeFeature(evt.feature);
@@ -109,37 +120,37 @@ function init(ulname,name,count){
             //console.log(format);
             //window.feature = evt.feature;
             //window.temp = format;
-            sketch=null;
+            sketch = null;
         });
     }
 
-    testType.onchange=function(e){
+    testType.onchange = function (e) {
         map.removeInteraction(draw2);
-        addInteraction2(map,testType,source);
+        addInteraction2(map, testType, source);
     };
-    var formateLength=function(line){
-        var length=Math.round(line.getLength()*100)/100;
+    var formateLength = function (line) {
+        var length = Math.round(line.getLength() * 100) / 100;
         var output;
-        if(length>100){
-            output=(Math.round(length/1000*100)/100)+' '+'km';
+        if (length > 100) {
+            output = (Math.round(length / 1000 * 100) / 100) + ' ' + 'km';
         }
-        else{
-            output=(Math.round(length*100)/100)+' '+'m';
+        else {
+            output = (Math.round(length * 100) / 100) + ' ' + 'm';
         }
         return output;
     };
-    var formateArea=function(polygon){
-        var area=polygon.getArea();
+    var formateArea = function (polygon) {
+        var area = polygon.getArea();
         var output;
-        if(area>10000){
-            output=(Math.round(area/100000*100)/100)+' '+ 'km<sup>2</sup>';
+        if (area > 10000) {
+            output = (Math.round(area / 100000 * 100) / 100) + ' ' + 'km<sup>2</sup>';
         }
-        else{
-            output=(Math.round(area*100)/100)+' '+'m<sup>2</sup>';
+        else {
+            output = (Math.round(area * 100) / 100) + ' ' + 'm<sup>2</sup>';
         }
         return output;
     };
 
-    setMarkUnFlod(ulname,name,count);
+    setMarkUnFlod(ulname, name, count);
 }
 
